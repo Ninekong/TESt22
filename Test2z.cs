c7e0d9adbe16280e2828d24073730cd0c2cd6bcf
Ninekong <122553014+Ninekong@users.noreply.github.com>
2023-04-03 10:27:12 +0700

    Add files via upload

---
+++ Test2z.cs
@@ -0,0 +1,12 @@
+﻿static void Main(string[] args) {
+    Console.Write("Input number 10.25: ");
+    float number1 = float.Parse(Console.ReadLine());
+    Console.Write("Input number 20; ");
+    float number2 = float.Parse(Console.ReadLine());
+    Console.Write("input number2; ");
+    float number3 =float.Parse(Console.ReadLine());
+    Console.Write("Input Opareter: ")
+    string inputopareter = Console.ReadLine();
+
+
+
